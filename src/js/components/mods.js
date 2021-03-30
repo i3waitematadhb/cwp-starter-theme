ea44eb7236889bbe7b464ba927c694f72f76e674
Keith L <k8t_loise16@yahoo.com>
2021-03-30 17:54:32 +1300

    Update theme

---
+++ src/js/components/mods.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import $ from 'jquery';
+
+
+export default function () {
+  $(document).ready(function ()
+  {
+    initialiseDocument();
+  });
+
+  function initialiseDocument()
+  {
+
+  }
+}
+
+/* eslint-enable */
